94f26c53570f895859d3603c44995e3b25b7dab3
mbriamon <mbriamon@nd.edu>
2025-02-05 12:46:33 -0500

    activity4

---
+++ activity4.js
@@ -0,0 +1,26 @@
+const graph = [
+	node : [],   //graph has a list of nodes
+	edge : {}   //needs to be an adjacency list
+
+	addNode(n) {
+		//check if node already exists (NO DUPLICATES)
+		//push node to end of list (queue)
+		//initialize empty adj list for node
+	},
+
+	addEdge(n1, n2) {
+		//add n1
+		//add n2
+		//add n1 to n2 edge
+		//add n2 to n1 edge
+	},
+
+	bfs(startNode){
+
+	},
+
+	dfs(startNode){
+
+	}
+
+}
