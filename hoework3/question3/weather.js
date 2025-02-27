1c473800ecd0ccee60c7f4a3703c761e9b24e2f2
mbriamon <130667100+mbriamon@users.noreply.github.com>
2025-02-26 20:06:28 -0500

    Update weather.js

--- hoework3/question3/weather.js
+++ hoework3/question3/weather.js
@@ -2,18 +2,24 @@ function toCelsius(){
 
 	// grabs the input from the user
 	let input =  document.getElementById("temperature").value;
-
-	let warning = document.getElementById("warning");
-
 	let resultParent = document.getElementById("result-parent");
 	let result = document.getElementById("result");
 
+	let currentWarning = document.getElementById("warning");
+	if (currentWarning) {
+		currentWarning.remove();
+	}
+	
 	//check if input is a valid number
 	if(isNaN(input) || input.trim === ""){
+		let warning = document.createElement("div");
+		warning.id = "warning";
+		
 		warning.innerText("Please input a valid number!");
 		warning.style.color("red");
 		warning.style.fontWeight("bold");
 		resultParent.style.visibility("hidden"); //hides result if the input is not a number
+		resultParent.ParentNode.insertBefore(warning, resultParent);
 	} else {
 		// converts the temperature to C
 		let celsius = (input - 32) * 5/9;
