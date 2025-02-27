52b435a1b1df33e95c3f48d5bb84cf3839d1abdc
mbriamon <130667100+mbriamon@users.noreply.github.com>
2025-02-26 20:02:02 -0500

    Update weather.js

--- hoework3/question3/weather.js
+++ hoework3/question3/weather.js
@@ -2,12 +2,25 @@ function toCelsius(){
 
 	// grabs the input from the user
 	let input =  document.getElementById("temperature").value;
-	// converts the temperature to C
-	let celsius = (input - 32) * 5/9;
 
-	// show back to the user, on the <span> element
-	document.getElementById("result").innerText = celsius;
-	// makes the div visible
-	// element.style can be used to change CSS properties of an HTML document
-	document.getElementById("result-parent").style.visibility = "visible";
+	let warning = document.getElementById("warning");
+
+	let resultParent = document.getElementById("result-parent");
+	let result = document.getElementById("result");
+
+	//check if input is a valid number
+	if(isNaN(input) || input.trim === ""){
+		warning.innerText("Please input a valid number!");
+		warning.style.color("red");
+		warning.style.fontWeight("bold");
+		resultParent.style.visibility("hidden"); //hides result if the input is not a number
+	} else {
+		// converts the temperature to C
+		let celsius = (input - 32) * 5/9;
+		// show back to the user, on the <span> element
+		document.getElementById("result").innerText = celsius;
+		// makes the div visible
+		// element.style can be used to change CSS properties of an HTML document
+		document.getElementById("result-parent").style.visibility = "visible";
+	}
 }
